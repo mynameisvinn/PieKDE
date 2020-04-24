9f0f6512eea84d6bdec91a80a4661f27aac18eb2
mynameisvinn <vin.tang@gmail.com>
2020-04-24 09:26:12 -0400

    turn methods into class

--- KDE.py
+++ KDE.py
@@ -1,29 +1,50 @@
 import math
 import numpy as np
 
-def _kernel(point, observation, bandwidth):
-    """
-    observation refers to the point where the kernel is centered. think of it as the
-    anchor point. 
-    
-    formula from https://medium.com/analytics-vidhya/kernel-density-estimation-kernel-construction-and-bandwidth-optimization-using-maximum-b1dfce127073
-    
-    code from https://stackoverflow.com/questions/12412895/calculate-probability-in-normal-distribution-given-mean-std-in-python
-    """
-    denom = bandwidth * ((2*math.pi)**.5)    
-    num = math.exp(-0.5 * ((point-observation)/bandwidth)**2)
-    return num/denom
+class KDE(object):
+    def __init__(self):
+        self.observations = 0
 
-def kde(observations, bandwidth):
-
-    delta = np.max(observations) - np.min(observations) 
-    low = np.min(observations) - delta
-    high = np.max(observations) + delta
+    def _kernel(self, point, observation, bandwidth):
+        """
+        each observation refers to the point where the gaussian is centered. 
     
-    k = high - low  # number of points
-    sum_densities = np.zeros(k)
-    for obs in observations:
-        sum_densities += np.array(list(
-            map(lambda pt: _kernel(pt, observation=obs, bandwidth=bandwidth), np.arange(low, high))))
-    sum_densities /= len(observations)
-    return sum_densities
+        formula from https://medium.com/analytics-vidhya/kernel-density-estimation-kernel-construction-and-bandwidth-optimization-using-maximum-b1dfce127073
+
+        code from https://stackoverflow.com/questions/12412895/calculate-probability-in-normal-distribution-given-mean-std-in-python
+        """
+        denom = bandwidth * ((2*math.pi)**.5)    
+        num = math.exp(-0.5 * ((point-observation)/bandwidth)**2)
+        return num/denom
+
+
+    def fit(self, observations, low, high, size, bandwidth):
+        self.observations = observations
+        self.bandwidth = bandwidth
+        
+        sum_densities = np.zeros(size)
+        for obs in observations:
+            sum_densities += np.array(list(
+                map(lambda pt: self._kernel(pt, observation=obs, bandwidth=bandwidth), np.linspace(low, high, size))))
+        sum_densities /= len(observations)
+        return sum_densities
+
+    
+    def sample(self, size):
+        """draw samples from a fitted KDE.
+
+        sampling from KDE can be viewed as uniform sampling of its gaussians.
+        """
+        if len(self.observations) == 0:
+            raise ValueError("need to fit KDE with observations")
+
+        samples = np.zeros(size)
+        for i in range(size):
+            
+            # randomly select an anchor point for a gaussian
+            anchor_point = np.random.choice(self.observations)
+
+            # then sample from that gaussian
+            sample = np.random.normal(loc=anchor_point, scale=self.bandwidth)
+            samples[i] = sample
+        return samples
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # PieKDE
-piekde is a simple implementation of a kernel density estimator. 
+piekde is a implementation of a kernel density estimator. 
 
 ## why kdes?
-you have a handful observations but not the true data generating function. you want this  distribution (or an approximation) so you can do things like generate new samples.
+you have a handful of observations but not the true data generating function `P(X)`. 
 
-## kdes are gmms but with lots of gaussians
-with gaussian mixture models, we have a k gaussians that emit observations, where k < n. with kdes, we assume each data point anchors a gaussian. (because we no longer need to specify the number of gaussians underlying the true data generating function, kdes are considered non-parametric.)
+you can approximate `P(x)` using a kde. you could then draw from this approximation for new samples (eg digits, faces).
+
+## kdes are gmms but with lots and lots of gaussians
+with gaussian mixture models, we have a k gaussians that emit observations, where k < n. 
+
+with kdes, we assume each data point represents a gaussian. because we no longer need to specify the number of gaussians for our approximating function, kdes are considered non-parametric.
